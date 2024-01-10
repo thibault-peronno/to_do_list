a436b5d7bdce6a8edd511d630bcd2ec4737972cd
Thib-numberX <peronnothibault@hotmail.fr>
2024-01-10 15:47:50 +0100

    change the auth middleware to flutter

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -4,9 +4,21 @@ import 'dotenv/config';
 const authMiddleware  = {
     checkToken: (req, res, next) => {
         try{
+            let token = '';
+            if( req.cookies?.auth_cookies){
+                console.log('res cookies', req.cookies?.auth_cookies);
+                token = req.cookies?.auth_cookies;
+            } else if(req.headers.authorization){
+                try {
+                    token = req.headers.authorization.split(' ')[1];
+                    console.log('headers token', token);
+                   } catch (error) {
+                    console.error('Error assigning token:', error);
+                   }
+            }
             // console.log(req.cookies);
-            const token = req.cookies?.auth_cookies;
             // const token = req.headers.authorization.split(' ')[1];
+            console.log('ligne 17', token);
             jwt.verify(token, process.env.JWT_SECRET);
             next();
         }catch{
