a429a8464c78f653ad03c7477ee3d9aadd740c08
Thib-numberX <peronnothibault@hotmail.fr>
2024-01-22 09:11:45 +0100

    test jest ok

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -1,23 +1,30 @@
-import authController from "../Controllers/AuthController";
-import authModel from "../Models/auth";
+import AuthController from "../Controllers/AuthController";
+import AuthModel from "../Models/auth";
+
+const authModel = new AuthModel();
+const authController = new AuthController();
+
+// const authController = require("../Controllers/AuthController");
+// const authModel = require("../Models/auth");
 
 describe('Model Test', function() {
     test('loginUser returns user', async () => {
+        console.log(authModel);
         const user = await authModel.loginUser('thib@gmail.com');
         expect(user).toBeDefined();
-        expect(user.identifiant).toBe('adminPassword123456789');
+        expect(user.identifiant).toBe('thib@gmail.com');
     });
 
     test('loginUser returns error', async () => {
         const user = await authModel.loginUser('invalidIdentifiant@gmail.com');
-        expect(user).toBeNull();
+        expect(user).toBeUndefined();
     });
 });
 
 describe('Controller Test', function() {
     test('login returns user', async () => {
         const req = { body: { identifiant: 'thib@gmail.com', password: 'adminPassword123456789' } };
-        const res = { json: jest.fn(), status: jest.fn().mockReturnThis() };
+        const res = { json: jest.fn(), status: jest.fn().mockReturnThis(), cookie: jest.fn(), };
         await authController.login(req, res);
         expect(res.status).toHaveBeenCalledWith(200);
         expect(res.json).toHaveBeenCalled();
