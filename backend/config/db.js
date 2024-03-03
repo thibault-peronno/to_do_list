3c24a29c51889b3152d9a22d03bfea06035f3d0f
Thib-numberX <peronnothibault@hotmail.fr>
2024-03-03 12:01:52 +0100

    work in progress

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv/config";
 let connectDB;
 
   connectDB = mysql.createConnection({
-    host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
+    host: process.env.HOST || 'mysqldb',
+    port: process.env.PORT || '3306',
+    user: process.env.USER_MYSQL || 'Thibault_to_do_list',
+    password: process.env.PASSWORD_MYSQL || 'Tberp876$39nGdeLp',
+    database: process.env.DATABASE_MYSQL || 'to_do_list',
     reconnect: true,
   });
 
