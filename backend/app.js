465f6f121c27159da9b17aa6dad7a343b37a85ae
Thib-numberX <peronnothibault@hotmail.fr>
2024-03-07 09:52:57 +0100

    online works

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["https://to-do-list.thibault-peronno.fr/"], credentials: true }));
 
 app.use(router);
 
