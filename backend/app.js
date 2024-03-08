48caea41fc6b8c5dd38b800e08593145114b3484
thibault-peronno <peronnothibault@hotmail.fr>
2024-03-08 14:33:38 +0100

    change uri

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: ["https://to-do-list.thibault-peronno.fr/"], credentials: true }));
+app.use(cors({ origin: ["https://to-do-list.thibault-peronno.fr"], credentials: true }));
 
 app.use(router);
 
